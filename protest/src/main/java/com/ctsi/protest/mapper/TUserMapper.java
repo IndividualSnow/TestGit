9ac599f70f4e2f0fca169667ba79009b1981154f
xuyilin <15565150903@136.com>
2021-01-15 15:59:14 +0800

    数据访问层

--- protest/src/main/java/com/ctsi/protest/mapper/TUserMapper.java
+++ protest/src/main/java/com/ctsi/protest/mapper/TUserMapper.java
@@ -1,7 +1,14 @@
 package com.ctsi.protest.mapper;
 
+import com.ctsi.protest.domain.TUser;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface TUserMapper {
+
+    @Select("SELECT * FROM t_user WHERE user_id = #{user_id}")
+    TUser getUser(Integer user_id);
+
+    TUser saveTUser();
 }
