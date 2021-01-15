cabf80c75a3f8b55a9e3115bbbc3bd9fe16ec6fa
xuyilin <15565150903@136.com>
2021-01-15 15:42:03 +0800

    用户对象的修改和删除

--- protest/src/main/java/com/ctsi/protest/service/TUserService.java
+++ protest/src/main/java/com/ctsi/protest/service/TUserService.java
@@ -9,4 +9,12 @@ public class TUserService {
     public TUser getUser() {
         return new TUser(1, 1, "张三", 23, 1);
     }
+
+    public TUser updeteUser(TUser user) {
+        return new TUser(2, 2, "李四", 23, 1);
+    }
+
+    public int deleteUser(Integer user_id) {
+        return 1;
+    }
 }
--- protest/src/main/java/com/ctsi/protest/web/TUserController.java
+++ protest/src/main/java/com/ctsi/protest/web/TUserController.java
@@ -1,8 +1,6 @@
 package com.ctsi.protest.web;
 
 import com.ctsi.protest.domain.TUser;
-import com.ctsi.protest.service.TUserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TUserController {
 
-    @Autowired
-    private TUserService tUserService;
-
     @GetMapping("/getUser")
     public TUser getUser() {
-        return tUserService.getUser();
+        return new TUser(1, 1, "张三", 23, 1);
     }
 }
